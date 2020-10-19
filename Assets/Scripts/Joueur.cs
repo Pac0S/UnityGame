e2d12609f269fcca8444f9ba7d41eea396d51fd3
Pac0o0 <paco.salomon@gmail.com>
2020-10-19 16:42:21 +0200

    Tapis roulant avec apparition sur clic et disparition

---
+++ Assets/Scripts/Joueur.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joueur : MonoBehaviour
+{
+
+    private int points = 0;
+    private int remainingLives = 3;
+    public GameObject[] projectiles;
+    private GameObject[] targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            System.Random rnd = new System.Random();
+            int projIndex = rnd.Next(2);
+            GameObject.Instantiate(projectiles[projIndex], new Vector3(-6, 0f, 0.0f), Quaternion.identity);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Projectile : MonoBehaviour
+{
+    public bool state; //Sur le tapis ou attrappé/envoyé
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Détruire l'objet si il dépasse une certaine position
+        if(transform.position.x >= 6)
+        {
+            Object.Destroy(this.gameObject);
+        }
+
+        Vector3 direction = -Vector3.Normalize(transform.position - new Vector3(6.0f, 0.0f, 0.0f));
+        direction.y = 0.0f;
+        direction.z = 0.0f;
+        transform.Translate(direction*5 * Time.deltaTime);
+        Debug.Log(transform.position.x);
+
+    }
+}
