5cfca4e4fcc8704a217b93d6b7bd5cd86ac1ed2b
Pac0o0 <paco.salomon@gmail.com>
2020-10-21 16:10:32 +0200

    correction target

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,28 +16,8 @@ public class Target : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          
-}
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Target : MonoBehaviour
-{
-
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.Rotate(new Vector3(90f, 0f, 0f));
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    
 }
+
 
